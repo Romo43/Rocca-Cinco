85323110eba08bdfaa76eb36d03474c0685c4ed2
Romo43 <59634716+Romo43@users.noreply.github.com>
2021-07-29 23:15:23 -0500

    a

--- src/models/menu.js
+++ src/models/menu.js
@@ -1,21 +1,11 @@
 const { Schema, model } = require("mongoose");
 
 const menuSchema = new Schema({
-    name: {
-        type: String
-    }, 
-    description: {
-        type: String
-    },
-    price: {
-        type: Number
-    },
-    type: {
-        type: String
-    },
-    URLimage: {
-        type: String
-    }
+    name: String,
+    description: String,
+    price: Number,
+    type: String,
+    URLimage: String
 }, {
     timestamps: true
 })
